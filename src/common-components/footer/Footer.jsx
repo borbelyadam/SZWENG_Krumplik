98539227d1142e0124bf3c33e191a1590895b4d6
iklevente <ilyokovacslevente@gmail.com>
2024-03-06 17:44:20 +0100

    refactor footer css

--- src/common-components/footer/Footer.jsx
+++ src/common-components/footer/Footer.jsx
@@ -1,34 +1,68 @@
 import { styled } from "@mui/material";
 import React from "react";
-import "./Footer.css";
 
 const StyledFooter = styled("footer")({
-  position: "absolute",
   bottom: 0,
   left: 0,
   right: 0,
   background: "aquamarine",
   height: "auto",
   width: "100vw",
-  fontFamily: "Open Sans",
-  paddingTop: "40px",
+  
   color: "#fff",
+});
+
+const FooterContent = styled("div")({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  flexDirection: "column",
+  textAlign: "center",
+});
+
+const FooterContentH3 = styled("h3")({
+  fontSize: "1.8rem",
+  fontWeight: 400,
+  textTransform: "capitalize",
+  lineHeight: "3rem",
+  color: "black",
+});
+
+const FooterContentP = styled("p")({
+  maxWidth: "500px",
+  margin: "10px auto",
+  lineHeight: "28px",
+  fontSize: "14px",
+  color: "black",
+});
+
+const FooterBottom = styled("div")({
+  background: "aquamarine",
+  width: "100vw",
+  padding: "20px 0",
+  textAlign: "center",
+});
+const FooterBottomP = styled("p")({
+  fontSize: "14px",
+  wordSpacing: "2px",
+  textTransform: "capitalize",
+  color: "black",
 });
 
 const Footer = () => {
   return (
     <>
       <StyledFooter>
-        <div className="footer-content">
-          <h3>evoFrontend</h3>
-          <p>
+        <FooterContent>
+          <FooterContentH3>evoFrontend</FooterContentH3>
+          <FooterContentP color="black">
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo iste
             corrupti doloribus odio sed!
-          </p>
-        </div>
-        <div className="footer-bottom">
-          <p>Copyright &copy; {new Date().getFullYear()} evoFrontend team.</p>
-        </div>
+          </FooterContentP>
+        </FooterContent>
+        <FooterBottom>
+          <FooterBottomP>Copyright &copy; {new Date().getFullYear()} evoFrontend team.</FooterBottomP>
+        </FooterBottom>
       </StyledFooter>
     </>
   );
