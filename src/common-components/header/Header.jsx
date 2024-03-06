cb8530ef47aaea6f9797ac72df00d39cdfb3ba49
kmike11 <k.michael1154@gmail.com>
2024-03-06 13:33:54 +0100

    Header refactoring

--- src/common-components/header/Header.jsx
+++ src/common-components/header/Header.jsx
@@ -1,23 +1,41 @@
 import React from "react";
-import HeaderButton from "./HeaderButton";
 import logo from "./evosoftlogo.png";
 import { useNavigate } from "react-router-dom";
 import MuiFormattedButton from "../MuiFormattedButton";
-
 const Header = (props) => {
   const navigate = useNavigate();
 
   const navigateToMainPage = () => {
     navigate("/");
   };
-
   const navigateToFaqPage = () => {
     navigate("/FAQ");
   };
-
+  const navigateToAboutUsPage = () => {
+    navigate("/About-Us");
+  };
+  const navigateToContactUsPage = () => {
+    navigate("/Contact");
+  };
+  const headerStyle = {
+    backgroundColor: "aquamarine",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  }
+  const searchFieldStyle = {
+    marginLeft: "30%",
+  }
+  const linkStyle = {
+    marginLeft:"auto",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: "80px",
+  }
   return (
     <>
-      <div className="header1">
+      <div className="header1" style={headerStyle}>
         <img
           src={logo}
           width="50"
@@ -25,20 +43,24 @@ const Header = (props) => {
           alt="logo"
           onClick={navigateToMainPage}
         />
-        <div className="search-field">
+        <div className="search-field" style={searchFieldStyle}>
           <input type="text" placeholder="Search" style={{ width: "400px" }} />
           <button>Search</button>
         </div>
 
-        <div className="links">
-          {/* <HeaderButton text="FAQ" path="/FAQ" /> */}
+        <div className="links" style={linkStyle}>
           <MuiFormattedButton
             buttonText="FAQ"
             onButtonClick={navigateToFaqPage}
           />
-
-          <HeaderButton text="About us" path="/About-Us" />
-          <HeaderButton text="Contact" path="/Contact" />
+          <MuiFormattedButton
+            buttonText="About Us"
+            onButtonClick={navigateToAboutUsPage}
+          />
+          <MuiFormattedButton
+            buttonText="Contact"
+            onButtonClick={navigateToContactUsPage}
+          />
           <button>Log in/Sign up</button>
         </div>
       </div>
