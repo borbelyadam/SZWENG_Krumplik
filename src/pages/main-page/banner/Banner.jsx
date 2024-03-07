a50fb6e06cfdf5b97f23bf4c1b043712573a2893
borbelyadam <borbelyadam0819@gmail.com>
2024-03-07 06:15:39 +0100

    Refactored Banner Css

--- src/pages/main-page/banner/Banner.jsx
+++ src/pages/main-page/banner/Banner.jsx
@@ -1,13 +1,70 @@
 import React from "react";
+import evoButtonL from "./evoButtonL.png"
+import evoButton from "./evoButton.png"
+
+
+const LeftButton =({
+  backgroundColor: "Transparent",
+  height: "400px",
+  width: "60px",
+  marginLeft: "25px",
+  border: "none",
+});
+
+const RightButton =({
+  backgroundColor: "Transparent",
+  height: "400px",
+  width: "60px",
+  marginRight: "25px",
+  float: "right",
+  border: "none",
+});
+
+const Banner1 =({
+  border: "0",
+  height: "400px",
+  width: "100%",
+  backgroundColor: "#00cc99"
+});
+
+const Banner2 =({
+  border: "0",
+  height: "400px",
+  width: "100%",
+  backgroundColor: "#ff0066"
+});
+
+
+const currentBanner = [Banner1,Banner2]
+
+let IndexCount = 0;
+
+
+const indexPlus = () => {
+  IndexCount++
+  
+}
+const indexMinus = () => {
+  IndexCount--
+}
 
 const Banner = () => {
   return (
     <>
-      <div style={{ border: "0", height: "400px", width: "100%" }}>
-        <button style={{ height: "400px", width: "60px" }}>left</button>
-        <button style={{ height: "400px", width: "60px", float: "right" }}>
-          right
+      <div id="BannerPic" style={currentBanner[IndexCount]}>
+        
+        <button style={LeftButton} onClick={indexMinus()}>
+        <img src={evoButtonL}
+        alt="Bal"
+        width={"60px"}/>
         </button>
+
+        <button style={RightButton} onClick={indexPlus()}>
+        <img src={evoButton}
+        alt="Jobb"
+        width={"60px"}/>
+        </button>
+
       </div>
     </>
   );
